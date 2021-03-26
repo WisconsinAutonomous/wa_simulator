1616fe67c3e04f05d768e1db2f93307fb15c39c3
AaronYoung5 <aarony3131@gmail.com>
2021-03-26 11:36:42 -0500

    Updated tests to pass

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,17 +24,16 @@ class TestWAUtils(unittest.TestCase):
     """Tests various package level things"""
 
     def test_data_directory(self):
-        """Tests the global DATA_DIRECTORY variable"""
+        """Tests the global get_wa_data_directory() variable"""
         import os.path
 
-        self.assertTrue(os.path.isdir(utils.DATA_DIRECTORY))
-        self.assertTrue(os.path.isfile(utils.DATA_DIRECTORY + '/test/test.json'))  # noqa
+        self.assertTrue(os.path.isdir(utils.get_wa_data_directory()))
+        self.assertTrue(os.path.isfile(utils.get_wa_data_directory() + '/test/test.json'))  # noqa
 
     def test_get_wa_data_file(self):
         """Tests the get_wa_data_file method"""
         import os.path
 
-        self.assertTrue(os.path.isfile(utils.get_wa_data_file('/test/test.json')))  # noqa
         self.assertTrue(os.path.isfile(utils.get_wa_data_file('test/test.json')))  # noqa
         self.assertFalse(os.path.isfile(utils.get_wa_data_file('test/test2.json')))  # noqa
 
@@ -42,55 +41,55 @@ class TestWAUtils(unittest.TestCase):
         """Tests the set_wa_data_directory method"""
         import os
 
-        old = utils.DATA_DIRECTORY
+        old = utils.get_wa_data_directory()
 
         # Absolute
         utils.set_wa_data_directory("/TEST")
-        self.assertTrue(utils.DATA_DIRECTORY == "/TEST")
+        self.assertTrue(utils.get_wa_data_directory() == "/TEST")
         self.assertTrue(old != "/TEST")
 
         # Relative
         utils.set_wa_data_directory("TEST")
-        self.assertTrue(utils.DATA_DIRECTORY == os.path.join(os.getcwd(), "TEST"))  # noqa
+        self.assertTrue(utils.get_wa_data_directory() == os.path.join(os.getcwd(), "TEST"))  # noqa
         self.assertTrue(utils.get_wa_data_file('test/test.json') == os.path.join(os.getcwd(), "TEST/test/test.json"))  # noqa
         self.assertFalse(utils.get_wa_data_file('test/test.json') == os.path.join(os.getcwd(), "TEST/test/test2.json"))  # noqa
 
         # Reset
         utils.set_wa_data_directory(old)
-        self.assertTrue(utils.DATA_DIRECTORY == old)
+        self.assertTrue(utils.get_wa_data_directory() == old)
 
     def test_set_wa_data_directory_temp(self):
         """Tests the set_wa_data_diretory_temp method"""
         import os
 
-        old = utils.DATA_DIRECTORY
+        old = utils.get_wa_data_directory()
 
         # Absolute
         with utils.set_wa_data_directory_temp("/TEST"):
-            self.assertTrue(utils.DATA_DIRECTORY == "/TEST")
+            self.assertTrue(utils.get_wa_data_directory() == "/TEST")
             self.assertTrue(old != "/TEST")
-        self.assertFalse(utils.DATA_DIRECTORY == "/TEST")
+        self.assertFalse(utils.get_wa_data_directory() == "/TEST")
 
         with utils.set_wa_data_directory_temp("TEST"):
-            self.assertTrue(utils.DATA_DIRECTORY == os.path.join(os.getcwd(), "TEST"))  # noqa
+            self.assertTrue(utils.get_wa_data_directory() == os.path.join(os.getcwd(), "TEST"))  # noqa
             self.assertTrue(utils.get_wa_data_file('test/test.json') == os.path.join(os.getcwd(), "TEST/test/test.json"))  # noqa
             self.assertFalse(utils.get_wa_data_file('test/test.json') == os.path.join(os.getcwd(), "TEST/test/test2.json"))  # noqa
-        self.assertTrue(utils.DATA_DIRECTORY == old)
+        self.assertTrue(utils.get_wa_data_directory() == old)
 
     def test_check_type(self):
         """Tests the check_type method"""
 
         with self.assertRaises(TypeError):
-            utils.check_type("TEST", int, "test", "test")
+            utils._check_type("TEST", int, "test", "test")
         with self.assertRaises(TypeError):
-            utils.check_type("TEST", bool, "test", "test")
+            utils._check_type("TEST", bool, "test", "test")
         with self.assertRaises(TypeError):
-            utils.check_type(True, str, "test", "test")
+            utils._check_type(True, str, "test", "test")
 
         # Doesn't raise
         try:
-            utils.check_type("TEST", str, "test", "test")
-            utils.check_type(True, bool, "test", "test")
+            utils._check_type("TEST", str, "test", "test")
+            utils._check_type(True, bool, "test", "test")
         except:
             self.fail("Raise exception unexpectedly!")
 
@@ -98,7 +97,7 @@ class TestWAUtils(unittest.TestCase):
         """Tests the load_json method"""
 
         try:
-            j = utils.load_json(utils.get_wa_data_file('test/test.json'))
+            j = utils._load_json(utils.get_wa_data_file('test/test.json'))
         except:
             self.fail("Raise exception unexpectedly!")
 
@@ -114,38 +113,38 @@ class TestWAUtils(unittest.TestCase):
 
     def test_check_field(self):
         """Tests the check_field method"""
-        j = utils.load_json(utils.get_wa_data_file('test/test.json'))
+        j = utils._load_json(utils.get_wa_data_file('test/test.json'))
 
         try:
-            utils.check_field(j, 'Name')
-            utils.check_field(j, 'Test', optional=True)
-            utils.check_field(j, 'Template', value='GPS')
-            utils.check_field(j, 'Properties', field_type=dict)
-            utils.check_field(j['Properties']['Noise Model'], 'Noise Type',
-                              field_type=str, allowed_values=['Normal', 'Test'])
+            utils._check_field(j, 'Name')
+            utils._check_field(j, 'Test', optional=True)
+            utils._check_field(j, 'Template', value='GPS')
+            utils._check_field(j, 'Properties', field_type=dict)
+            utils._check_field(j['Properties']['Noise Model'], 'Noise Type',
+                               field_type=str, allowed_values=['Normal', 'Test'])
         except:
             self.fail("Raise exception unexpectedly!")
 
         with self.assertRaises(KeyError):
-            utils.check_field(j, "Test")
+            utils._check_field(j, "Test")
         with self.assertRaises(TypeError):
-            utils.check_field(j, "Name", field_type=bool)
+            utils._check_field(j, "Name", field_type=bool)
         with self.assertRaises(ValueError):
-            utils.check_field(j, "Name", value='Noise Model')
+            utils._check_field(j, "Name", value='Noise Model')
         with self.assertRaises(ValueError):
-            utils.check_field(j, "Name", value='Noise Model', optional=True)
+            utils._check_field(j, "Name", value='Noise Model', optional=True)
 
     def test_check_field_allowed_values(self):
         """Tests the check_field_allowed_values method"""
-        j = utils.load_json(utils.get_wa_data_file('test/test.json'))
+        j = utils._load_json(utils.get_wa_data_file('test/test.json'))
 
         try:
-            utils.check_field_allowed_values(j, 'Name', ['Test GPS Sensor Model', 'Test', 'Type'])  # noqa
+            utils._check_field_allowed_values(j, 'Name', ['Test GPS Sensor Model', 'Test', 'Type'])  # noqa
         except:
             self.fail("Raise exception unexpectedly!")
 
         with self.assertRaises(ValueError):
-            utils.check_field_allowed_values(j, 'Name', ['Test', 'Type'])  # noqa
+            utils._check_field_allowed_values(j, 'Name', ['Test', 'Type'])  # noqa
 
 
 if __name__ == '__main__':
